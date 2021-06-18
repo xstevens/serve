8785728928cdf607cb4b34759d386ad7f68e699f
Xavier Stevens <xstevens@fastly.com>
2021-06-17 17:31:31 -0700

    Upgrading to Rocket 0.5-rc1 and Rust 2018 async

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,67 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
-#[macro_use] extern crate clap;
+#[macro_use]
+extern crate clap;
 extern crate itertools;
-#[macro_use] extern crate rocket;
-extern crate time;
+#[macro_use]
+extern crate rocket;
+extern crate chrono;
 
-use std::fs;
-use std::io;
-use std::io::Write;
 use std::path::{Path, PathBuf};
 
+use chrono::{DateTime, SecondsFormat, Utc};
 use clap::{App, Arg};
-use rocket::Data;
-use rocket::config::{Config, Environment, LoggingLevel};
+use rocket::config::Config;
+use rocket::data::{Data, ToByteUnit};
 use rocket::fairing::AdHoc;
+use rocket::fs::NamedFile;
 use rocket::http::Header;
-use rocket::response::NamedFile;
+use rocket::log::LogLevel;
 use rocket::response::content;
+use rocket::tokio;
+use tokio::fs::File;
+use tokio::io::{self, AsyncWriteExt};
 
 #[get("/ping")]
 fn ping() -> &'static str {
     "OK\r\n"
 }
 
+#[get("/robots.txt")]
+fn robots() -> &'static str {
+    "User-agent: *\r\nDisallow: /\r\n"
+}
+
 #[get("/static/<path..>")]
-fn files(path: PathBuf) -> Option<NamedFile> {
+async fn files(path: PathBuf) -> Option<NamedFile> {
     let path = Path::new("./static").join(path);
-    NamedFile::open(&path).ok()
+    NamedFile::open(&path).await.ok()
 }
 
 #[post("/upload/<path..>", data = "<data>", rank = 10)]
-fn upload(path: PathBuf, data: Data) -> io::Result<()> {
+async fn upload(path: PathBuf, data: Data<'_>) -> std::io::Result<()> {
     let fpath = Path::new("./upload/").join(path);
     if let Some(dir) = fpath.parent() {
         if !dir.exists() {
-            fs::create_dir_all(&dir)?;
+            std::fs::create_dir_all(&dir)?;
         }
     }
-    data.stream_to_file(&fpath)?;
+    let mut file = File::create(&fpath).await?;
+
+    data.open(1.mebibytes()).stream_to(&mut file).await?;
+
     Ok(())
 }
 
-#[post("/", data = "<data>", rank = 1)]
-fn dump(data: Data) -> io::Result<()> {
-    let stdout = io::stdout();
-    let mut writer = stdout.lock();
-    data.stream_to(&mut writer)?;
-    writer.write(b"\n")?;
+#[post("/dump", data = "<data>", rank = 1)]
+async fn dump(data: Data<'_>) -> io::Result<()> {
+    let mut stdout = rocket::tokio::io::stdout();
+    data.open(1.mebibytes()).stream_to(&mut stdout).await?;
+    stdout.write(b"\n").await?;
 
     Ok(())
 }
 
 #[catch(404)]
-fn not_found(_req: &rocket::Request) -> content::Html<String> { 
+fn not_found(_req: &rocket::Request) -> content::Html<String> {
     content::Html(
 r#"<!DOCTYPE html>
 <html>
@@ -66,11 +75,12 @@ r#"<!DOCTYPE html>
         <p>The requested resource could not be found.</p>
     </div>
 </body>
-</html>"#.to_owned()
+</html>"#.to_owned(),
     )
 }
 
-fn main() {
+#[launch]
+fn rocket() -> rocket::Rocket<rocket::Build> {
     let args = App::new("serve")
         .version(crate_version!())
         .about("a static HTTP server")
@@ -82,70 +92,95 @@ fn main() {
                 .takes_value(true),
         )
         .arg(
-           Arg::with_name("key")
+            Arg::with_name("key")
                 .long("key")
                 .value_name("KEY")
                 .help("path to TLS private key")
                 .takes_value(true),
         )
+        .arg(
+            Arg::with_name("port")
+                .long("port")
+                .value_name("PORT")
+                .default_value("8000")
+                .help("port number to listen on")
+                .takes_value(true),
+        )
         .get_matches();
+
     // configuration
-    let mut config_builder = Config::build(Environment::Production)
-                                .address("0.0.0.0")
-                                .port(8000)
-                                .log_level(LoggingLevel::Off);
+    let port: u16 = args.value_of("port").unwrap().parse().unwrap_or(8000);
+    let mut config = Config::figment()
+        .merge(("port", port))
+        .merge(("address", "0.0.0.0"))
+        .merge(("log_level", LogLevel::Off))
+        .merge(("cli_colors", false));
     if args.is_present("cert") && args.is_present("key") {
-        config_builder = config_builder.tls(args.value_of("cert").unwrap(), args.value_of("key").unwrap());
+        config = config
+            .merge(("tls.certs", args.value_of("cert").unwrap()))
+            .merge(("tls.key", args.value_of("key").unwrap()));
     }
-    let config = config_builder.finalize().unwrap();
+
     // setup rocket with custom fairing for request logging
     rocket::custom(config)
         .attach(AdHoc::on_request("request_log", |req, _| {
-            let ts = time::strftime("%Y-%m-%dT%H:%M:%S.%fZ", &time::now_utc()).unwrap();
+            Box::pin(async move {
+                let now: DateTime<Utc> = Utc::now();
+                let remote_addr: String = match req.remote() {
+                    Some(addr) => format!("{}", addr.ip()),
+                    _ => "-".to_owned(),
+                };
 
-            let remote_addr: String = match req.remote() {
-                Some(addr) => format!("{}", addr.ip()),
-                _ => "-".to_owned(),
-            };
-
-            let referrer: &str = match req.headers().get_one("Referer") {
-                Some(referer) => {
-                    if referer.len() == 0 {
-                        "-"
-                    } else {
-                        referer
+                let referrer: &str = match req.headers().get_one("Referer") {
+                    Some(referer) => {
+                        if referer.len() == 0 {
+                            "-"
+                        } else {
+                            referer
+                        }
                     }
-                },
-                _ => "-",
-            };
+                    _ => "-",
+                };
 
-            let user_agent: &str = match req.headers().get_one("User-Agent") {
-                Some(ua) => ua,
-                _ => "-",
-            };
+                let user_agent: &str = match req.headers().get_one("User-Agent") {
+                    Some(ua) => ua,
+                    _ => "-",
+                };
 
-            let mut cookies = itertools::join(req.headers().get("Cookie"), ",");
-            if cookies.len() == 0 {
-                cookies = "-".to_owned();
-            }
+                let mut cookies = itertools::join(req.headers().get("Cookie"), ",");
+                if cookies.len() == 0 {
+                    cookies = "-".to_owned();
+                }
 
-            let authz: &str = match req.headers().get_one("Authorization") {
-                Some(authz_header) => {
-                    if authz_header.len() == 0 {
-                        "-"
-                    } else {
-                        authz_header
+                let authz: &str = match req.headers().get_one("Authorization") {
+                    Some(authz_header) => {
+                        if authz_header.len() == 0 {
+                            "-"
+                        } else {
+                            authz_header
+                        }
                     }
-                },
-                _ => "-",
-            };
+                    _ => "-",
+                };
 
-            println!("{} {} {} {} \"{}\" \"{}\" \"{}\" \"{}\"", ts, remote_addr, req.method(), req.uri(), referrer, user_agent, cookies, authz);
+                println!(
+                    "{} {} {} {} \"{}\" \"{}\" \"{}\" \"{}\"",
+                    now.to_rfc3339_opts(SecondsFormat::Millis, true),
+                    remote_addr,
+                    req.method(),
+                    req.uri(),
+                    referrer,
+                    user_agent,
+                    cookies,
+                    authz
+                );
+            })
         }))
         .attach(AdHoc::on_response("server_response_header", |_, resp| {
-            resp.set_header(Header::new("Server", "NeXTcube"));
+            Box::pin(async move {
+                resp.set_header(Header::new("Server", "NeXTcube"));
+            })
         }))
-        .mount("/", routes![ping, files, upload, dump])
-        .register(catchers![not_found])
-        .launch();
+        .mount("/", routes![ping, robots, files, upload, dump])
+        .register("/", catchers![not_found])
 }
